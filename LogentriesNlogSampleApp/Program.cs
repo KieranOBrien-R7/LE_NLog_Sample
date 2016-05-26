915d44a32a2d578d202f3e1d54575c31519f60dd
Kieran O'Brien <kieran.obrien@logentries.com>
2016-05-26 14:36:35 +0100

    First commit

---
+++ LogentriesNlogSampleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace LogentriesNlogSampleApp
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (true)
+            {
+                logger.Debug(n);
+                logger.Info(n+1);
+                logger.Warn(n+2);
+                n = n + 3;
+            }
+            String a = System.Console.ReadLine();
+        }
+    }
+}
